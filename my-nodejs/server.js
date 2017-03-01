d22835dbda5384419a5794c0322973ba6931c804
root <root@cloud.islief.com>
2017-03-01 11:33:56 +0000

    fixed duplicate createServer

--- my-nodejs/server.js
+++ my-nodejs/server.js
@@ -1,15 +1,6 @@
 var http = require('http');
 
-http.createServer(function(request, response) {
-  var headers = request.headers;
-  var method = request.method;
-  var url = request.url;
-  var body = [];
-
-  var server = http.createServer(function (request, response) {
+http.createServer(function (request, response) {
     response.writeHead(200, {'Content-Type': 'text/html'});
     response.end('<html><body><div style="position:absolute;top:50%;left:0;margin-top:-50px;right:0;text-align: center;font-family: Arial, Helvetica, sans-serif;color: green;font-size: 40px;">Hello, World! ... brought to you by NodeJS</div></body></html>');
-  }
-});
-
-server.listen(8002);
+}).listen(8002);
