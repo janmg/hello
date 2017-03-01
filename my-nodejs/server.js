1aa25eebbcabfc4e3221ea37a7e7dcb45091e154
Jan <jan.m.geertsma@gmail.com>
2017-03-01 15:17:04 +0000

    nodejs fixes

--- my-nodejs/server.js
+++ my-nodejs/server.js
@@ -1,6 +1,7 @@
 var http = require('http');
+var response = "<html><body><div style="position:absolute;top:50%;left:0;margin-top:-50px;right:0;text-align: center;font-family: Arial, Helvetica, sans-serif;color: green;font-size: 40px;">Hello, World! ... brought to you by NodeJS</div></body></html>";
 
 http.createServer(function (request, response) {
-    response.writeHead(200, {'Content-Type': 'text/html'});
-    response.end('<html><body><div style="position:absolute;top:50%;left:0;margin-top:-50px;right:0;text-align: center;font-family: Arial, Helvetica, sans-serif;color: green;font-size: 40px;">Hello, World! ... brought to you by NodeJS</div></body></html>');
+    response.writeHead(200, {'Content-Type': 'text/html'},{'Connection': 'keep-alive'},{'Content-Length': response.length});
+    response.end(response);
 }).listen(8002);
