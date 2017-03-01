b7ccf23c3cbe1d65f482fcb5b354193c0deac692
Jan <jan.m.geertsma@gmail.com>
2017-03-01 10:21:09 +0000

    updated applications

--- my-golang/hello-world.go
+++ my-golang/hello-world.go
@@ -6,7 +6,7 @@ import (
 )
 
 func all(w http.ResponseWriter, r *http.Request) {
-	io.WriteString(w, "<html><body><div style='position:absolute;top:50%;left:40;margin-top:-50px;right:-40;text-align: center;font-family: Arial, Helvetica, sans-serif;color: orange;font-size: 40px;'>Hello, World! ... brought to you by GO</div></body></html>")
+	io.WriteString(w, "<html><body><div style='position:absolute;top:50%;left:0;margin-top:-50px;right:0;text-align: center;font-family: Arial, Helvetica, sans-serif;color: orange;font-size: 40px;'>Hello, World! ... brought to you by GO</div></body></html>")
 }
 
 func main() {
--- my-nodejs/server.js
+++ my-nodejs/server.js
@@ -6,15 +6,10 @@ http.createServer(function(request, response) {
   var url = request.url;
   var body = [];
 
-  request.on('error', function(err) {
-    console.error(err);
-  }).on('data', function(chunk) {
-    body.push(chunk);
-  }).on('end', function() {
-    body = Buffer.concat(body).toString();
-
+  var server = http.createServer(function (request, response) {
     response.writeHead(200, {'Content-Type': 'text/html'});
-    response.end('<html><body><div style="position:absolute;top:50%;left:40;margin-top:-50px;right:-40;text-align: center;font-family: Arial, Helvetica, sans-serif;color: green;font-size: 40px;">Hello, World! ... brought to you by NodeJS</div></body></html>');
-  });
-}).listen(8002);
+    response.end('<html><body><div style="position:absolute;top:50%;left:0;margin-top:-50px;right:0;text-align: center;font-family: Arial, Helvetica, sans-serif;color: green;font-size: 40px;">Hello, World! ... brought to you by NodeJS</div></body></html>');
+  }
+});
 
+server.listen(8002);
