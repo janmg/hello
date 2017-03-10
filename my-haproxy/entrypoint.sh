c38051cd6fb7bc614c805532f807eee016fc5e5a
Jan <jan.m.geertsma@gmail.com>
2017-03-10 12:23:48 +0000

    updated haproxy

--- my-haproxy/entrypoint.sh
+++ my-haproxy/entrypoint.sh
@@ -1,15 +1,22 @@
 #!/bin/sh
-set -e
+#https://github.com/million12/docker-haproxy/blob/master/container-files/bootstrap.sh
 
-# first arg is `-f` or `--some-option`
-if [ "${1#-}" != "$1" ]; then
-	set -- haproxy "$@"
-fi
+set -u
 
-if [ "$1" = 'haproxy' ]; then
-	# if the user wants "haproxy", let's use "haproxy-systemd-wrapper" instead so we can have proper reloadability implemented by upstream
-	shift # "haproxy"
-	set -- "$(which haproxy-systemd-wrapper)" -p /run/haproxy.pid "$@"
-fi
+HAPROXY_CONFIG=${HAPROXY_CONFIG:="/etc/haproxy/haproxy.cfg"}
+HAPROXY_PID_FILE="/var/run/haproxy.pid"
+HAPROXY_CMD="haproxy -f ${HAPROXY_CONFIG} -p ${HAPROXY_PID_FILE}"
+HAPROXY_CHECK_CONFIG_CMD="haproxy -f ${HAPROXY_CONFIG} -c"
 
-exec "$@"
+${HAPROXY_CHECK_CONFIG_CMD}
+${HAPROXY_CMD}
+# Exit immidiately in case of any errors or when we have interactive terminal
+if [[ $? != 0 ]] || test -t 0; then exit $?; fi
+
+
+while inotifywait -q -e create,delete,modify,attrib ${HAPROXY_CONFIG}; do
+  ${HAPROXY_CHECK_CONFIG_CMD} > /dev/null
+  if [ $? -eq 0 ]; then
+    ${HAPROXY_CMD} -sf $(cat ${HAPROXY_PID_FILE})
+  fi
+done
