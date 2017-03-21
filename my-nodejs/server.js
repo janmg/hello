4f991dadfeaecb4a924882da49ef82167a42b1e0
Jan <jan.m.geertsma@gmail.com>
2017-03-21 14:43:57 +0000

    Remove my- prefix from nodejs

--- my-nodejs/server.js
+++
@@ -1,7 +0,0 @@
-var http = require('http');
-var response = "<html><body><div style="position:absolute;top:50%;left:0;margin-top:-50px;right:0;text-align: center;font-family: Arial, Helvetica, sans-serif;color: green;font-size: 40px;">Hello, World! ... brought to you by NodeJS</div></body></html>";
-
-http.createServer(function (request, response) {
-    response.writeHead(200, {'Content-Type': 'text/html'},{'Connection': 'keep-alive'},{'Content-Length': response.length});
-    response.end(response);
-}).listen(8002);
