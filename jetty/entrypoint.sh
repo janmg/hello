318c4105db4dba32f7e45b1f829c0bcc2b384da3
janmg <jan.m.geertsma@gmail.com>
2018-02-14 13:17:26 +0000

    refreshed jetty

--- jetty/entrypoint.sh
+++ jetty/entrypoint.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-java -cp jetty-all-uber.jar:hello.jar com.islief.HelloWorld 8003
+setfattr -n user.pax.flags -v "mr" /usr/bin/java
+/usr/bin/java -cp HelloJetty-1.0.1.jar:http2-common.jar:http2-hpack.jar:http2-server.jar:javax.servlet-api.jar:jetty-http.jar:jetty-io.jar:jetty-server.jar:jetty-util.jar com.islief.HelloWorld 8003
--- jetty/src/main/java/com/islief/HelloWorld.java
+++ jetty/src/main/java/com/islief/HelloWorld.java
@@ -17,26 +17,24 @@ import org.eclipse.jetty.http2.server.HTTP2CServerConnectionFactory;
 public class HelloWorld extends AbstractHandler
 {
     @Override
-    public void handle( String target,
-                        Request baseRequest,
-                        HttpServletRequest request,
-                        HttpServletResponse response ) throws IOException,
-                                                      ServletException
+    public void handle( String target, Request baseRequest, HttpServletRequest request, HttpServletResponse response ) throws IOException, ServletException
     {
-        response.setContentType("text/html; charset=utf-8");
         response.setStatus(HttpServletResponse.SC_OK);
+        response.setContentType("text/html; charset=utf-8");
         response.getWriter().println("<html><body><div style='position:absolute;top:50%;left:0;margin-top:-50px;right:0;text-align: center;font-family: Arial, Helvetica, sans-serif;color: blue;font-size: 40px;'>Hello, World! ... brought to you by Jetty</div></body></html>");
         baseRequest.setHandled(true);
     }
 
     public static void main(String[] args) throws Exception
     {
+        int port = 8080;
+        if (args.length > 0) {port = new Integer(args[0]);}
         Server server = new Server();
         HttpConfiguration config = new HttpConfiguration();
         HttpConnectionFactory http1 = new HttpConnectionFactory(config);
         HTTP2CServerConnectionFactory http2c = new HTTP2CServerConnectionFactory(config); 
         ServerConnector connector = new ServerConnector(server, http1, http2c);
-        connector.setPort(8003);
+        connector.setPort(port);
         server.addConnector(connector);
 
         server.setHandler(new HelloWorld());
