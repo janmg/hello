75b6959061645e127983b01d0c276542246ea046
janmg <jan.m.geertsma@gmail.com>
2022-05-14 12:27:30 +0300

    Update hello-world.py

--- python/hello-world.py
+++ python/hello-world.py
@@ -6,8 +6,11 @@ class Handler(http.server.SimpleHTTPRequestHandler):
     def do_GET(self):
         self.send_response(HTTPStatus.OK)
         self.end_headers()
-        self.wfile.write(b'Hello world')
+        self.wfile.write(b"<html><head><link rel='stylesheet' type='text/css' href='/style.css' /></head><body><div id='main'>Hello, World! ... brought to you by Python</div></body></html>")
+
+# "Content-Type", "text/html; charset=utf-8"
+# "Content-Type", "text/css; charset=utf-8"
+# #main { position:absolute;top:50%;left:0;margin-top:-50px;right:0;text-align: center;font-family: Lato;color: #3572A5;font-size: 40px; }
 
 httpd = socketserver.TCPServer(('', 8080), Handler)
 httpd.serve_forever()
-
