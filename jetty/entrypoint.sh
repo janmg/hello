8bc27b008a049d5d3a5ef2bfa8b7fdd9f5fa535a
Jan Geertsma <jan@janmg.com>
2025-01-25 11:13:27 +0200

    housekeeping

--- jetty/entrypoint.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-setfattr -n user.pax.flags -v "mr" /usr/bin/java
-/usr/bin/java -cp "*" com.islief.HelloWorld 8003
--- nodejs/entrypoint.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-setfattr -n user.pax.flags -v "mr" /usr/bin/node
-npm start
